711cb2235b543133d8bc78fd45b5740f5833b42e
mujin <mujinzhang@rdchina.net>
2023-05-07 16:58:35 +0800

    查看页面

--- src/pages/lookchoukuanPage/index.vue
+++ src/pages/lookchoukuanPage/index.vue
@@ -3,9 +3,62 @@
     <rs-nav-bar title="发起筹款" @back="back" />
     <view class="group_3 flex-col" :class="{close:!visible1}">
       <view class="box_7 flex-row justify-between" @click="changeVisible('visible1')">
+        <text class="text_3">志愿者信息</text>
+        <uni-icons class="arrorR" type="top" color="#999" size="15"></uni-icons>
+      </view>
+      <text class="text_4">社工</text>
+      <view class="box_2 flex-col"></view>
+      <text class="text_4">社工</text>
+      <view class="box_2 flex-col"></view>
+      <view class="box_8 flex-row justify-between">
+        <view class="group_4 flex-col">
+          <image
+            @click="clickImg('https://img1.baidu.com/it/u=1357002844,4027162325&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=465',0)"
+            class="image_4" mode="aspectFit"
+            referrerpolicy="no-referrer"
+            src="https://img1.baidu.com/it/u=1357002844,4027162325&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=465" />
+        </view>
+        <view class="group_4 flex-col">
+          <image
+            @click="clickImg('https://picnew9.photophoto.cn/20150510/xuankufeixiangdeyingwutupian-09960611_1.jpg',1)"
+            class="image_4" mode="aspectFit"
+            referrerpolicy="no-referrer"
+            src="https://picnew9.photophoto.cn/20150510/xuankufeixiangdeyingwutupian-09960611_1.jpg" />
+        </view>
+
+      </view>
+    </view>
+    <view class="group_3 flex-col" :class="{close:!visible1}">
+      <view class="box_7 flex-row justify-between" @click="changeVisible('visible1')">
         <text class="text_3">老人基础信息</text>
         <uni-icons class="arrorR" type="top" color="#999" size="15"></uni-icons>
       </view>
+
+      <view class="box_8 flex-row justify-between">
+        <view class="group_4 flex-col">
+          <image
+            @click="clickImg('https://img1.baidu.com/it/u=1357002844,4027162325&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=465',0)"
+            class="image_4" mode="aspectFit"
+            referrerpolicy="no-referrer"
+            src="https://img1.baidu.com/it/u=1357002844,4027162325&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=465" />
+        </view>
+        <view class="group_4 flex-col">
+          <image
+            @click="clickImg('https://picnew9.photophoto.cn/20150510/xuankufeixiangdeyingwutupian-09960611_1.jpg',1)"
+            class="image_4" mode="aspectFit"
+            referrerpolicy="no-referrer"
+            src="https://picnew9.photophoto.cn/20150510/xuankufeixiangdeyingwutupian-09960611_1.jpg" />
+        </view>
+
+      </view>
+
+    </view>
+    <view class="group_3 flex-col" :class="{close:!visible1}">
+      <view class="box_7 flex-row justify-between" @click="changeVisible('visible1')">
+        <text class="text_3">辅助证明</text>
+        <uni-icons class="arrorR" type="top" color="#999" size="15"></uni-icons>
+      </view>
+
       <view class="box_8 flex-row justify-between">
         <view class="group_4 flex-col">
           <image
