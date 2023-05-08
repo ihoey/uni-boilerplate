bbbeb7e43b799776d33c7e93218ac5eeafb38960
mujin <mujinzhang@rdchina.net>
2023-05-08 16:53:34 +0800

    支付成功

---
+++ src/pages/paySuccess/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <view class="page flex-col">
+    <view class="block_1 flex-col">
+      <view class="box_1 flex-row">
+        <text class="text_10">9:14</text>
+        <image
+          class="label_3"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngab1f668ef389f8d1eabe926edde5b79edcd3b25e5eef9ee6f587487a865052d1" />
+        <image
+          class="label_4"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPnga8787b58a7be32d2cf2201bba25050f496df02953514fa23ffe6e318b971df2a" />
+        <image
+          class="image_5"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngfdda9b02b45b47979d3cbb2d1d480fa7011b1fb979a2ca1f815538d25b2d4dfb" />
+      </view>
+      <view class="nav-bar_2 flex-row justify-between">
+        <image
+          class="icon_1"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng4098ca5f4c0f8815e4c844ae226de41806bb74c652d8e02f13a3614eaef60958" />
+        <image
+          class="applet-top-bar_1"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng9b3de2a04d8c71a024ded39dac87a33d9bcb39c3d781be45f3717c0c25c5dd0e" />
+      </view>
+      <view class="box_2 flex-row justify-between">
+        <image
+          class="icon_2"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngaa905040bee0ffe3f604ab1f62c1dbf6793b7a2e633488c4330db6263127c25b" />
+        <text class="text_2">帮助成功</text>
+      </view>
+      <text class="text_3">
+        感谢您对老人无私的帮助，请与他人共同分享您的爱心吧
+      </text>
+      <view class="box_3 flex-row justify-between">
+        <button class="button_1 flex-col" @click="onClick_1">
+          <text class="text_4">下载证书</text>
+        </button>
+        <button class="button_2 flex-col" @click="onClick_2">
+          <text class="text_5">立即分享</text>
+        </button>
+      </view>
+      <view class="group_4 flex-col">
+        <view class="image-wrapper_1 flex-col">
+          <image
+            class="image_2"
+            referrerpolicy="no-referrer"
+            src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPnge39716a762495938526522300c18959cc3c49bb93dad8f6153a220353cad4af4" />
+        </view>
+      </view>
+      <view class="image-wrapper_2 flex-col">
+        <image
+          class="image_3"
+          referrerpolicy="no-referrer"
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng6765fa5b922f692613bad2635f026155475f18cc5758e14698159da0fa0865d3" />
+      </view>
+      <image
+        class="image_4"
+        referrerpolicy="no-referrer"
+        src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng3bdfdc76e87ec9cf41b606afeba9850434cc290461e81f2bdeea946431780a9e" />
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  name: "component_name",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
